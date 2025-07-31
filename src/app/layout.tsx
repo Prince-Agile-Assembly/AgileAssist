e939d45dbb4fdfcd28894a8777b837134df602fc
HARISH KUMAR S P PSVP CSE III <harishkumar.s.p.27.cse@psvpec.in>
2025-07-31 19:29:37 +0000

    api.puter.com/whoami:1  Failed to load resource: the server responded wi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
         <Script
-          src="https://js.puter.com/v2/"
+          src="https://js.puter.com/v2/?guest"
           strategy="beforeInteractive"
           onLoad={() => {
             window.dispatchEvent(new Event('puter-loaded'));
